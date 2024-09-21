47d2f05e2450bfc250210d6cf5a88e002b3b7795
Piyush Mali <piyushmali702@gmail.com>
2024-09-22 00:31:43 +0530

    contract setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,43 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",  // For OpenZeppelin contracts
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200  // Adjust the number of runs based on your use case
+          }
+        }
+      },
+      {
+        version: "0.6.6",   // For UniswapV2Router02 and SafeMath in UniswapV2
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.5.16",  // For UniswapV2Factory, UniswapV2Pair, etc.
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ]
+  },
+  
+  networks: {
+    polygonAmoy: {
+      url: `https://polygon-amoy.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
